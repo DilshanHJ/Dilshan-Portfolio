67d48d4703763fcee96597742f559ab651051b20
Dilshan Hashitha <hashithadiljay@gmail.com>
2025-05-18 05:45:02 +0530

    More Projects are added

--- src/data/projects.js
+++ src/data/projects.js
@@ -54,6 +54,32 @@ import aemt4 from "./../assets/images/AEMT/4.png";
 import aemt5 from "./../assets/images/AEMT/5.png";
 import aemt6 from "./../assets/images/AEMT/6.png";
 import aemt7 from "./../assets/images/AEMT/7.png";
+import gameEngine1 from "./../assets/images/GameEngine/1.png";
+import gameEngine2 from "./../assets/images/GameEngine/2.png";
+import proPathSim1 from "./../assets/images/ProjectilePathSimulator/1.png";
+import proPathSim2 from "./../assets/images/ProjectilePathSimulator/2.png";
+import mobileParking1 from "./../assets/images/MobileParking/1.png";
+import ODM1 from "./../assets/images/OrderDeliveryManagment/1.png";
+import ODM2 from "./../assets/images/OrderDeliveryManagment/2.png";
+import ODM3 from "./../assets/images/OrderDeliveryManagment/3.png";
+import ODM4 from "./../assets/images/OrderDeliveryManagment/4.png";
+import ODM5 from "./../assets/images/OrderDeliveryManagment/5.png";
+import ODM6 from "./../assets/images/OrderDeliveryManagment/6.png";
+import ODM7 from "./../assets/images/OrderDeliveryManagment/7.png";
+import PM1 from "./../assets/images/ProjectManagement/1.png";
+import PM2 from "./../assets/images/ProjectManagement/2.png";
+import PM3 from "./../assets/images/ProjectManagement/3.png";
+import UL1 from "./../assets/images/UniLanka/1.png";
+import UL2 from "./../assets/images/UniLanka/2.png";
+import UL3 from "./../assets/images/UniLanka/3.png";
+import UL4 from "./../assets/images/UniLanka/4.png";
+import UL5 from "./../assets/images/UniLanka/5.png";
+import UL6 from "./../assets/images/UniLanka/6.png";
+import UL7 from "./../assets/images/UniLanka/7.png";
+import UL8 from "./../assets/images/UniLanka/8.png";
+import UL9 from "./../assets/images/UniLanka/9.png";
+import UL10 from "./../assets/images/UniLanka/10.png";
+import SMM1 from "./../assets/images/SharedMoneyManager/1.png";
 
 export const projects = [
   {
@@ -62,7 +88,7 @@ export const projects = [
     description:
       "Demonstrating OOP principles, OpenGL rendering using JOGL, including a custom animation engine to replace default FPS animators for smoother performance.",
     technologies: ["Java", "JOGL", "OpenGL", "Game Development", "Animation"],
-    images: ["https://placehold.co/400x200/1f2937/e2e8f0?text=No+Image"],
+    images: [gameEngine1, gameEngine2],
     github: "https://github.com/DilshanHJ/game-engine-java-opengl",
     demo: null,
     category: "game",
@@ -72,7 +98,7 @@ export const projects = [
     title: "One-way ANOVA Analyzer",
     description:
       "A statistical tool built with Java and NetBeans IDE to calculate ANOVA tests.",
-    technologies: ["Java", "GUI", "Statistics", "NetBeans"],
+    technologies: ["Java", "GUI", "Statistics", "NetBeans", "ANOVA"],
     images: [anova1],
     github: "https://github.com/DilshanHJ/one-way-anova-analyzer",
     demo: null,
@@ -83,7 +109,14 @@ export const projects = [
     title: "One-way ANOVA (Web Version)",
     description:
       "Converted to a web version using JavaScript, HTML, and CSS with jQuery implementation.",
-    technologies: ["JavaScript", "HTML", "CSS", "jQuery", "Statistics"],
+    technologies: [
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "jQuery",
+      "Statistics",
+      "ANOVA",
+    ],
     images: [anovaweb1, anovaweb2],
     github: "https://github.com/DilshanHJ/one-way-anova-web",
     demo: null,
@@ -94,7 +127,15 @@ export const projects = [
     title: "Results Analyzer",
     description:
       "System to manage student profiles, results, GPA calculations, and Dean's list reports.",
-    technologies: ["ExpressJS", "EJS", "Node.js", "Database"],
+    technologies: [
+      "ExpressJS",
+      "EJS",
+      "Node.js",
+      "Database",
+      "MySQL",
+      "RBAC",
+      "Auth Through Session tokens",
+    ],
     images: [
       resultAnalyzer1,
       resultAnalyzer2,
@@ -110,7 +151,14 @@ export const projects = [
     title: "FASNET Mobile App",
     description:
       "Mobile application integrates a results analyzer, LMS, and other student services into a unified system.",
-    technologies: ["Flutter", "Dart", "ExpressJS", "MySQL", "Mobile App"],
+    technologies: [
+      "Flutter",
+      "Dart",
+      "ExpressJS",
+      "MySQL",
+      "Mobile App",
+      "NodeJs",
+    ],
     images: [
       fasnet1,
       fasnet2,
@@ -143,7 +191,13 @@ export const projects = [
     title: "Attendance Data Management System",
     description:
       "Front-end interface for the ADMS used by Biometrix Pvt. Ltd. to manage and visualize attendance data.",
-    technologies: ["Vue.js", "Front-end", "Data Visualization"],
+    technologies: [
+      "Vue.js",
+      "Front-end",
+      "Data Visualization",
+      "Manual Authentication",
+      "RBAC",
+    ],
     images: [adms1, adms2, adms3, adms4, adms5, adms6, adms7],
     github: null,
     demo: null,
@@ -154,7 +208,19 @@ export const projects = [
     title: "Hotel Management System",
     description:
       "Full-stack solution for Biometrix Pvt. Ltd. includes OTA integration, POS, billing system, room and rate management, housekeeping, restaurant management and support for multiple room types and pricing plans.",
-    technologies: ["Vue.js", "Laravel", "Full-stack", "POS", "Billing"],
+    technologies: [
+      "Vue.js",
+      "Laravel",
+      "Full-stack",
+      "POS",
+      "Billing",
+      "Api Integration",
+      "Channel Manager",
+      "PMS",
+      "Jwt",
+      "RBAC",
+      "MVCRS",
+    ],
     images: [
       hotelpms1,
       hotelpms2,
@@ -198,6 +264,99 @@ export const projects = [
       "Final Year Project",
     ],
     images: [aemt1, aemt2, aemt3, aemt4, aemt5, aemt6, aemt7],
+    demo: null,
+    category: "software",
+  },
+  {
+    id: 10,
+    title: "Projectile Path Simulator with Java and OpenGL",
+    description:
+      "Demonstrating OOP principles, OpenGL rendering using JOGL,Using a custome made simple game engine to demonstrait implementation of simple motion physics in game",
+    technologies: [
+      "Java",
+      "JOGL",
+      "OpenGL",
+      "Game Development",
+      "Animation",
+      "Ingame Physics",
+    ],
+    images: [proPathSim1, proPathSim2],
+    github: "https://github.com/DilshanHJ/game-engine-java-opengl",
+    demo: null,
+    category: "game",
+  },
+  {
+    id: 11,
+    title: "Mobile Parking",
+    description:
+      "A Web-Based dashboard for a access controll and billing system of a parking lot that can be manage the billing,gate controll and vehicle tracking using a mobile app",
+    technologies: [
+      "Web-based",
+      "CodeIgniter",
+      "MySQL",
+      "Jwt",
+      "VueJs",
+      "CoreUI",
+    ],
+    images: [mobileParking1],
+    demo: null,
+    category: "software",
+  },
+  {
+    id: 12,
+    title: "Order Delivery Management System",
+    description:
+      "A Web-Based dashboard track orders, warehouses,delivary vehicals, billing of orders in realtime",
+    technologies: [
+      "Web-based",
+      "CodeIgniter",
+      "MySQL",
+      "Jwt",
+      "VueJs",
+      "CoreUI",
+    ],
+    images: [ODM1, ODM2, ODM3, ODM4, ODM5, ODM6, ODM7],
+    github: "https://github.com/DilshanHJ/Order-Delivery-Management-System",
+    demo: null,
+    category: "software",
+  },
+  {
+    id: 13,
+    title: "Project Management System",
+    description:
+      "A Web-Based System for project management that can track projects tasks and sub tasks, assign tasks to persons and more using Laravel 11 and ReactJs",
+    technologies: [
+      "Web-based",
+      "CodeIgniter",
+      "MySQL",
+      "Jwt",
+      "VueJs",
+      "CoreUI",
+    ],
+    images: [PM1, PM2, PM3],
+    github: "https://github.com/DilshanHJ/Project-Management-System",
+    demo: null,
+    category: "software",
+  },
+  {
+    id: 14,
+    title: "Uni Lanka",
+    description:
+      "A Web-Based System Example for managing all data related to different departments of sri lanka",
+    technologies: ["Web-based", "ExpressJs", "MongoDB", "ReactJs"],
+    images: [UL1, UL2, UL3, UL4, UL5, UL6, UL7, UL8, UL9, UL10],
+    github: "https://github.com/DilshanHJ/Project-Management-System",
+    demo: null,
+    category: "software",
+  },
+  {
+    id: 15,
+    title: "Shared Money Manager",
+    description:
+      "A Web-Based System that can manage day-to-day incomes and expenses of a group of people, debts and loan managemnt and more..",
+    technologies: ["Web-based", "ExpressJs", "MongoDB", "ReactJs"],
+    images: [SMM1],
+    github: "https://github.com/DilshanHJ/Project-Management-System",
     demo: null,
     category: "software",
   },
